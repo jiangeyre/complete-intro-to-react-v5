182c75d5735aa73f8680c74d1cb54e1301464bfe
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-11-28 19:21:11 -0800

    Commit 3

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
 import { Link } from '@reach/router';
-import { css } from '@emotion/core';
+import { css, keyframes } from '@emotion/core';
 
+const spin = keyframes`
+    to {
+        transform: rotate(360deg);
+    }
+`;
 
-const NavBar = () => {}
+const NavBar = () => {
     const [padding, setPadding] = useState(15);
 
     const color = "pink";
 
     return(
     <header
-        onClick={() => setPadding(padding + 15)}
         css={css`
             background-color: ${color};
             padding: ${padding}px;
@@ -20,6 +24,13 @@ const NavBar = () => {}
         <span
             css={css`
                 font-size: 60px;
+
+                display: inline-block;
+                animation: 1s ${spin} linear infinite reverse;
+
+                &:hover {
+                    text-decoration: underline;
+                }
             `} 
             role="img" aria-label="logo">🐱</span>
     </header>
