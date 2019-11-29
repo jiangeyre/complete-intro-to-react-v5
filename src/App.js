7b2508ae30d7f537a39313f0cdc8eb4be2c40a12
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-11-28 19:37:26 -0800

    Commit 4: Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@reach/router";
-import Details from "./Details";
+// import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
-import NavBar from "./NavBar";
+// import NavBar from "./NavBar";
+
+const Details = lazy(() => import('./Details'));
+//dynamic import
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <NavBar />  
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header> 
+        <Suspense fallback={<h1>loading route...</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
